a1bb84f839bc021e616cd42048e238020fc7dcba
Dan Kelly <dbkelly@dbk-dev-workstation.fios-router.home>
2016-07-18 16:31:48 -0400

    Wrapping up changes to the service. Removed functional tests since they did not work properly. Added documentation to methods, added additional tests, renamed a few things for clarity. Implementation complete.

--- state_server.py
+++ state_server.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python2.7
-
 import json
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
-class Coordinates(object):
+class PointInState(object):
 
     FILE_NAME = 'states.json'
 
-    '''I found this code snippet in a pure python implementation
-    at the following location. Essentially it takes a polygon and x,y coordinates
-     and returns a boolean based on whether the point lies within. I decided to reuse this for
-    convenience. http://www.ariel.com.au/a/python-point-int-poly.html'''
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
     def point_inside_polygon(self, x, y, poly):
+        """For convenience, I decided to reuse this point-in-polygon method that
+        I found using a pure python implementation at the location below.
+        x,y are longitude and latitude points
+        poly is a list of coordinates that form a polygon.
+        Returns a boolean based on whether the point lies within.
+        http://www.ariel.com.au/a/python-point-int-poly.html
+        """
+
         n = len(poly)
         inside = False
 
@@ -30,60 +37,99 @@ class Coordinates(object):
 
         return inside
 
-    def parse_states_json_for_match(self, longitude, latitude):
+    def get_state_from_point(self):
+        """
+        Opens and parses the states.json file to determine
+        if a given point falls between the border coordinates.
+        If we are going off of ISO 6709: https://en.wikipedia.org/wiki/ISO_6709
+        then longitude should be passed as the x value and latitude will be the y value.
+        :returns the name of the state based off given lat/lon otherwise None
+        """
         with open(self.FILE_NAME) as states:
             for line in states:
                 data = json.loads(line)
-                is_point_in_state = self.point_inside_polygon(latitude, longitude, list(data['border']))
+                is_point_in_state = self.point_inside_polygon(self.longitude, self.latitude, list(data['border']))
                 if is_point_in_state:
                     return data['state']
-
-    def get_state_from_request(self, post_data):
-        lat_long = self.get_lat_long_dict(post_data)
-        return self.parse_states_json_for_match(float(lat_long['latitude']), float(lat_long['longitude']))
-
-    def get_lat_long_dict(self, post_data):
-        try:
-            return dict(value.split('=') for value in post_data.split("&"))
-        except ValueError:
-            return None
 
 
 class StateServer(BaseHTTPRequestHandler):
 
     def _set_headers_success(self):
+        """
+        Sets the headers for a successful operation.
+        Returns a 200 response to the user.
+        """
         self.response = self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def _set_headers_failure(self):
+        """
+        Sets the headers for a failure scenario.
+        Returns a 400 response to the user.
+        """
         self.response = self.send_response(400)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+    def set_failure(self, msg):
+        """
+        Calls the set header method and writes a failure message to the user.
+        :param msg: The failure message displayed to the user.
+        """
+        self._set_headers_failure()
+        self.wfile.write(msg)
+
     def do_POST(self):
+        """
+        Handles POST requests sent to http://localhost:8080
+        If the data from the request cannot be parsed or if
+        no state is found for the given lat/lon then a 400
+        is returned. Otherwise, if successful, the name of the
+        state is returned.
+        """
         content_length = int(self.headers['Content-Length'])
         request = self.rfile.read(content_length)
-        coordinates = Coordinates()
-        state = coordinates.get_state_from_request(request)
+        lat_long = PointUtils().parse_request_for_lat_long(request)
+        if not lat_long:
+            self.set_failure('Invalid request. Proper format is "latitude=xx.xxx&longitude=-yy.yyy".')
+            return
+        point = PointInState(lat_long['latitude'], lat_long['longitude'])
+        state = point.get_state_from_point()
+        if not state:
+            self.set_failure('No state found for given coordinates.')
+            return
         self._set_headers_success()
         self.wfile.write(state)
 
     def do_GET(self):
-        self._set_headers_success()
-        self.wfile.write("Psst. Send me a post.")
+        """
+        No get available, display a 404.
+        """
+        self.send_error(404)
+        return
 
 
-def run(server_class=HTTPServer, handler_class=StateServer, port=8080):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting server...'
-    httpd.serve_forever()
+class PointUtils(object):
+
+    def parse_request_for_lat_long(self, post_data):
+        """
+        Utility method used to return the latitude and longitude as a dictionary
+        :param post_data: the request from the HTTP POST
+        :return: a lat/lon dict or None if an error occurs.
+        """
+        try:
+            lat_long = dict(value.split('=') for value in post_data.split('&'))
+            lat_long['latitude'] = float(lat_long['latitude'])
+            lat_long['longitude'] = float(lat_long['longitude'])
+            return lat_long
+        except ValueError:
+            return None
+        except KeyError:
+            return None
 
 if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+    server = HTTPServer(('localhost', 8080), StateServer)
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
--- unit_tests.py
+++ unit_tests.py
@@ -1,40 +1,80 @@
 import unittest
 
-from state_server import Coordinates
+from state_server import PointInState
+from state_server import PointUtils
 
 
 class UnitTests(unittest.TestCase):
 
-    def test_point_in_polygon(self):
-        state_coordinates = [[-77.475793, 39.719623], [-80.524269, 39.721209], [-80.520592, 41.986872], [-74.705273, 41.375059], [-75.142901, 39.881602], [-77.475793, 39.719623]]
-        coordinates = Coordinates()
-        point_in_state = coordinates.point_inside_polygon(-77.036133, 40.513799, state_coordinates)
-        self.assertEquals(point_in_state, True)
+    def test_point_in_polygon_true(self):
+        state_border = [[-77.475793, 39.719623], [-80.524269, 39.721209], [-80.520592, 41.986872], [-74.705273, 41.375059], [-75.142901, 39.881602], [-77.475793, 39.719623]]
+        point = PointInState(40.513799, -77.036133)
+        is_point_in_state = point.point_inside_polygon(point.longitude, point.latitude, state_border)
+        self.assertEqual(is_point_in_state, True)
 
-    def test_read_json_file_pa(self):
-        coordinates = Coordinates()
-        state = coordinates.parse_states_json_for_match(40.513799, -77.036133)
+    def test_point_in_polygon_false(self):
+        state_border = [[-77.475793, 39.719623], [-80.524269, 39.721209], [-80.520592, 41.986872], [-74.705273, 41.375059], [-75.142901, 39.881602], [-77.475793, 39.719623]]
+        point = PointInState(37.129269, -85.085177)
+        is_point_in_state = point.point_inside_polygon(point.longitude, point.latitude, state_border)
+        self.assertEqual(is_point_in_state, False)
+
+    def test_parse_request_for_lat_long(self):
+        request = "latitude=40.513799&longitude=-77.036133"
+        lat_long = PointUtils().parse_request_for_lat_long(request)
+        self.assertEqual(lat_long['latitude'], 40.513799)
+        self.assertEqual(lat_long['longitude'], -77.036133)
+
+    def test_parse_request_for_lat_long_blank_value(self):
+        request = "latitude=&longitude="
+        lat_long = PointUtils().parse_request_for_lat_long(request)
+        assert lat_long is None
+
+    def test_parse_request_for_lat_long_half_value(self):
+        request = "latitude=40.513799"
+        lat_long = PointUtils().parse_request_for_lat_long(request)
+        assert lat_long is None
+
+    def test_parse_request_for_lat_long_missing_value(self):
+        request = "latitude=40.513799&longitude="
+        lat_long = PointUtils().parse_request_for_lat_long(request)
+        assert lat_long is None
+
+    def test_parse_request_for_lat_long_invalid_value(self):
+        request = "blah blah blah hello& =world"
+        lat_long = PointUtils().parse_request_for_lat_long(request)
+        assert lat_long is None
+
+    """Test a few random points along state borders and within states to check for accuracy"""
+
+    def test_get_state_from_point_pa(self):
+        point = PointInState(40.513799, -77.036133)
+        state = point.get_state_from_point()
         self.assertEqual(state, 'Pennsylvania')
 
-    def test_read_json_file_ok(self):
-        coordinates = Coordinates()
-        state = coordinates.parse_states_json_for_match(35.485162, -96.893323)
+    def test_get_state_from_point_ok(self):
+        point = PointInState(35.485162, -96.893323)
+        state = point.get_state_from_point()
         self.assertEqual(state, 'Oklahoma')
 
-    def test_read_json_file_wv(self):
-        coordinates = Coordinates()
-        state = coordinates.parse_states_json_for_match(39.511637, -80.645768)
+    def test_get_state_from_point_wv(self):
+        point = PointInState(39.511637, -80.645768)
+        state = point.get_state_from_point()
         self.assertEqual(state, 'West Virginia')
 
-    def test_read_json_file_sd(self):
-        coordinates = Coordinates()
-        state = coordinates.parse_states_json_for_match(44.414159, -103.514929)
+    def test_get_state_from_point_sd(self):
+        point = PointInState(44.414159, -103.514929)
+        state = point.get_state_from_point()
         self.assertEqual(state, 'South Dakota')
 
-    def test_read_json_file_nv(self):
-        coordinates = Coordinates()
-        state = coordinates.parse_states_json_for_match(35.978121, -114.834509)
+    def test_get_state_from_point_nv(self):
+        point = PointInState(35.978121, -114.834509)
+        state = point.get_state_from_point()
         self.assertEqual(state, 'Nevada')
+
+    def test_get_state_from_point_az(self):
+        point = PointInState(34.233012, -114.122451)
+        state = point.get_state_from_point()
+        self.assertEqual(state, 'Arizona')
 
 
 if __name__ == '__main__':
